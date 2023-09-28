41fcf745090e237d0eb4c4d423d3aa6b123bda66
1123Alessandro <alessandroandyrei@gmail.com>
2023-09-28 20:35:33 +0800

    Hotfix q6

--- q6.sql
+++ q6.sql
@@ -1,4 +1,1 @@
-create view uniq as
-select count(*) from year group by year_sem, year_schyr;
-
-select count(*) as Combinations from uniq;
+select year_sem, year_schyr from year group by year_sem, year_schyr;
