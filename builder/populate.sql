ecbcbc240e2fc47f8da89ccd579afd31c976a5c8
1123Alessandro <alessandroandyrei@gmail.com>
2023-10-05 19:00:30 +0800

    Working copy of populate.sql

--- builder/populate.sql
+++ builder/populate.sql
@@ -21,7 +21,7 @@ CREATE TABLE student (
     stud_fam_income INT,
     stud_schlr_name VARCHAR(45),
     stud_schlr_type VARCHAR(45),
-    stud_hs_name VARCHAR(45),
+    stud_hs_name VARCHAR(90),
     stud_hs_street VARCHAR(45),
     stud_hs_country VARCHAR(45),
     stud_hs_grade INT,
@@ -81,36 +81,36 @@ CREATE TABLE enrollment (
 ----------------------------------------------------------------------------------------------------
 
 INSERT INTO student (stud_fname, stud_mname, stud_lname, stud_gender, stud_bday, stud_street, stud_city, stud_country, stud_ees, stud_app_fchoice, stud_app_schoice, stud_mother, stud_father, stud_fam_income, stud_schlr_name, stud_schlr_type, stud_hs_name, stud_hs_street, stud_hs_country, stud_hs_grade) VALUES
-("Sandra", "Rodrigo", "Bullok", "Female", "13-Nov-80", "Navarra Street", "Quezon City", "Philippines", 92, "Bachelor of Science in Pharmacy", "Bachelor of Science in Computer Science", "Elsa H. Rodrigo", "Narciso A. Bullok", 1000000, "Entrance Merit Scholarship", "Merit-based", "Mataas na Paaralan ng Mababang Bundok", "Mababang Bundok Street", "Philippines", 92),
-("Whammy", "Hieu", "Jackman", "Male", "2-Jan-02", "Espana Avenue", "Quezon City", "Philippines", 77, "Bachelor of Science in Physics", "Bachelor of Science in Computer Science", "Tran A. Hieu", "Hugh C. Jackman", 250000, "POEA Scholarship", "Needs-based", "Mataas na Paaralan ng Mababang Bundok", "Mababang Bundok Street", "Philippines", 80),
-("Olivia", "Rodrigo", "Duterte", "Female", "13-Mar-90", "P. Florentino Street", "Manila City", "Philippines", 80, "Bachelor of Science in Physics", "Bachelor of Science in Physical Education", "Elsa H. Rodrigo", "Rudy E. Duterte", 335000, "DOST Scholarship", "Merit-based", "Mataas na Paaralan ng Lungsod ng Maynila", "Mababang Bundok Street", "Philippines", 98),
-("Taylor", "Lautner", "Sheesh", "Non-Binary", "10-Jun-95", "Bagumbuhay Street", "Manila City", "Philippines", 84, "Bachelor of Arts in Filipino", "Bachelor of Arts in Communication Arts", "Margeary Z. Lautner", "Tom Z. Sheesh", 2600000, "nan", "nan", "International School of the National Artistic Arts University", "International Street", "Philippines", 70),
-("Zendaya", "Holland", "Park", "Female", "10-Sep-92", "Matalino Street", "Makati City", "Philippines", 90, "Bachelor of Science in Computer Science", "Bachelor of Arts in Filipino", "Alexandra X. Holland", "Jing-nae Park", 15000000, "nan", "nan", "International School of the National Artistic Arts University", "International Street", "Philippines", 75),
-("Zack", "Tabudlo", "Apron", "Male", "10-Dec-01", "Maginhawa Street", "Makati City", "Philippines", 95, "Bachelor of Science in Computer Science", "Bachelor of Arts in Filipino", "Matutina P. Tabudlo", "Keech N. Apron", 150000, "DOST Scholarship", "Needs-based", "Mataas na Paaralan ng Mababang Bundok", "Mababang Bundok Street", "Philippines", 90),
-("Ariana", "Falls", "Venti", "Female", "14-Feb-87", "L'oreal Street", "Makati City", "Philippines", 92, "Bachelor of Arts in Literature", "Bachelor of Science in Pharmacy", "Baga T. Falls", "Cinco H. Venti", 7000000, "nan", "nan", "International School of the National Artistic Arts University", "International Street", "Philippines", 92);
+("Sandra", "Rodrigo", "Bullok", "Female", "1980-11-13", "Navarra Street", "Quezon City", "Philippines", 92, "Bachelor of Science in Pharmacy", "Bachelor of Science in Computer Science", "Elsa H. Rodrigo", "Narciso A. Bullok", 1000000, "Entrance Merit Scholarship", "Merit-based", "Mataas na Paaralan ng Mababang Bundok", "Mababang Bundok Street", "Philippines", 92),
+("Whammy", "Hieu", "Jackman", "Male", "2002-01-02", "Espana Avenue", "Quezon City", "Philippines", 77, "Bachelor of Science in Physics", "Bachelor of Science in Computer Science", "Tran A. Hieu", "Hugh C. Jackman", 250000, "POEA Scholarship", "Needs-based", "Mataas na Paaralan ng Mababang Bundok", "Mababang Bundok Street", "Philippines", 80),
+("Olivia", "Rodrigo", "Duterte", "Female", "1990-03-13", "P. Florentino Street", "Manila City", "Philippines", 80, "Bachelor of Science in Physics", "Bachelor of Science in Physical Education", "Elsa H. Rodrigo", "Rudy E. Duterte", 335000, "DOST Scholarship", "Merit-based", "Mataas na Paaralan ng Lungsod ng Maynila", "Mababang Bundok Street", "Philippines", 98),
+("Taylor", "Lautner", "Sheesh", "Non-Binary", "1995-06-10", "Bagumbuhay Street", "Manila City", "Philippines", 84, "Bachelor of Arts in Filipino", "Bachelor of Arts in Communication Arts", "Margeary Z. Lautner", "Tom Z. Sheesh", 2600000, "nan", "nan", "International School of the National Artistic Arts University", "International Street", "Philippines", 70),
+("Zendaya", "Holland", "Park", "Female", "1992-09-10", "Matalino Street", "Makati City", "Philippines", 90, "Bachelor of Science in Computer Science", "Bachelor of Arts in Filipino", "Alexandra X. Holland", "Jing-nae Park", 15000000, "nan", "nan", "International School of the National Artistic Arts University", "International Street", "Philippines", 75),
+("Zack", "Tabudlo", "Apron", "Male", "2001-12-10", "Maginhawa Street", "Makati City", "Philippines", 95, "Bachelor of Science in Computer Science", "Bachelor of Arts in Filipino", "Matutina P. Tabudlo", "Keech N. Apron", 150000, "DOST Scholarship", "Needs-based", "Mataas na Paaralan ng Mababang Bundok", "Mababang Bundok Street", "Philippines", 90),
+("Ariana", "Falls", "Venti", "Female", "1987-02-14", "L'oreal Street", "Makati City", "Philippines", 92, "Bachelor of Arts in Literature", "Bachelor of Science in Pharmacy", "Baga T. Falls", "Cinco H. Venti", 7000000, "nan", "nan", "International School of the National Artistic Arts University", "International Street", "Philippines", 92);
 
 INSERT INTO admission (adm_year, adm_sem, adm_pgm_name, adm_pgm_desc, adm_pgm_units) VALUES
-("2001", 1, "Bachelor of Science in Computer Science", "Aims to provide knowledge in Computer Science", 37),
-("2001", 1, "Bachelor of Science in Physics", "Aims to provide knowledge in Physics", 39),
-("2002", 1, "Bachelor of Science in Computer Science", "Aims to provide knowledge in Computer Science", 37),
-("2002", 1, "Bachelor of Science in Physics", "Aims to provide knowledge in Physics", 39);
+("2001-01-01", 1, "Bachelor of Science in Computer Science", "Aims to provide knowledge in Computer Science", 37),
+("2001-01-01", 1, "Bachelor of Science in Physics", "Aims to provide knowledge in Physics", 39),
+("2002-01-01", 1, "Bachelor of Science in Computer Science", "Aims to provide knowledge in Computer Science", 37),
+("2002-01-01", 1, "Bachelor of Science in Physics", "Aims to provide knowledge in Physics", 39);
 
 INSERT INTO college (coll_name, coll_building, coll_dean, coll_deptaff, coll_deptchair) VALUES
 ("College of Information and Computing Sciences", "Blessed Pio Georgio Frassati", "Juan Luna", "Department of Computer Science", "Jose Rizal"),
 ("College of Science", "Main Building", "Apolinario Mabini", "Department of Physics", "Andres Bonifacio");
 
-INSERT INTO course (crs_code, crs_currver, crs_name, crs_units, crs_desc, crs_prereq, crs_type, crs_group, crs_instr, crs_sem, crs_schyr, crs_bldg_name, crs_bldg_room) VALUES
-("1996", "CS110", "Theory of Computation", 3, "Studies the theory of computation", "CS109", "Lecture", "Professional", "Henry Yrneh", 1, "2002", "Blessed Pio Georgio Frassati", "Room A"),
-("1996", "PHYS110", "Statistical Mechanics", 3, "Studies statistical mechanics", "PHYS109", "Laboratory", "Professional", "Albert Trebla", 2, "2002", "Main Building", "Room B"),
-("2001", "CS110", "Theory of Computation", 3, "Studies the theory of computation", "CS109", "Lecture", "Professional", "Henry Yrneh", 1, "2002", "Blessed Pio Georgio Frassati", "Room A"),
-("2001", "PHYS110", "Statistical Mechanics", 3, "Studies statistical mechanics", "PHYS109", "Laboratory", "Professional", "Albert Trebla", 2, "2002", "Main Building", "Room B"),
-("2001", "PHYS110", "Statistical Mechanics", 3, "Studies statistical mechanics", "PHYS109", "Laboratory", "Professional", "Madeline Eniledam", 2, "2002", "Main Building", "Room B"),
-("1996", "PHYS112", "Quantum Mechanics", 3, "Studies quantum mechanics", "PHYS111", "Lecture", "Professional", "Albert Trebla", 1, "2001", "Main Building", "Room C"),
-("2001", "CS109", "Algorithms and Complexity", 3, "Studies algorithms and complexity", "CS108", "Lecture", "Professional", "Henry Yrneh", 1, "2001", "Blessed Pio Georgio Frassati", "Room A"),
-("2001", "PE4", "Physical Education 4", 3, "Studies Physical Education", "PE3", "Laboratory", "General", "Albus Subla", 2, "2001", "Main Building", "Field A"),
-("2001", "RW2", "Works of Rizal", 3, "Studies the Works of Rizal", "RW1", "Lecture", "General", "Henry Yrneh", 2, "2001", "Blessed Pio Georgio Frassati", "Room D"),
-("1996", "FIL23", "Filipino Literature", 3, "Studies Philippine Literature", "CS109", "Lecture", "General", "Josie Eisoj", 2, "2003", "Blessed Pio Georgio Frassati", "Room D"),
-("1996", "PHYS112", "Quantum Mechanics", 3, "Studies quantum mechanics", "PHYS111", "Lecture", "Professional", "Albert Trebla", 1, "2001", "Main Building", "Room B");
+INSERT INTO course (crs_currver, crs_code, crs_name, crs_units, crs_desc, crs_prereq, crs_type, crs_group, crs_instr, crs_sem, crs_schyr, crs_bldg_name, crs_bldg_room) VALUES
+("1996-01-01", "CS110", "Theory of Computation", 3, "Studies the theory of computation", "CS109", "Lecture", "Professional", "Henry Yrneh", 1, "2002-01-01", "Blessed Pio Georgio Frassati", "Room A"),
+("1996-01-01", "PHYS110", "Statistical Mechanics", 3, "Studies statistical mechanics", "PHYS109", "Laboratory", "Professional", "Albert Trebla", 2, "2002-01-01", "Main Building", "Room B"),
+("2001-01-01", "CS110", "Theory of Computation", 3, "Studies the theory of computation", "CS109", "Lecture", "Professional", "Henry Yrneh", 1, "2002-01-01", "Blessed Pio Georgio Frassati", "Room A"),
+("2001-01-01", "PHYS110", "Statistical Mechanics", 3, "Studies statistical mechanics", "PHYS109", "Laboratory", "Professional", "Albert Trebla", 2, "2002-01-01", "Main Building", "Room B"),
+("2001-01-01", "PHYS110", "Statistical Mechanics", 3, "Studies statistical mechanics", "PHYS109", "Laboratory", "Professional", "Madeline Eniledam", 2, "2002-01-01", "Main Building", "Room B"),
+("1996-01-01", "PHYS112", "Quantum Mechanics", 3, "Studies quantum mechanics", "PHYS111", "Lecture", "Professional", "Albert Trebla", 1, "2001-01-01", "Main Building", "Room C"),
+("2001-01-01", "CS109", "Algorithms and Complexity", 3, "Studies algorithms and complexity", "CS108", "Lecture", "Professional", "Henry Yrneh", 1, "2001-01-01", "Blessed Pio Georgio Frassati", "Room A"),
+("2001-01-01", "PE4", "Physical Education 4", 3, "Studies Physical Education", "PE3", "Laboratory", "General", "Albus Subla", 2, "2001-01-01", "Main Building", "Field A"),
+("2001-01-01", "RW2", "Works of Rizal", 3, "Studies the Works of Rizal", "RW1", "Lecture", "General", "Henry Yrneh", 2, "2001-01-01", "Blessed Pio Georgio Frassati", "Room D"),
+("1996-01-01", "FIL23", "Filipino Literature", 3, "Studies Philippine Literature", "CS109", "Lecture", "General", "Josie Eisoj", 2, "2003-01-01", "Blessed Pio Georgio Frassati", "Room D"),
+("1996-01-01", "PHYS112", "Quantum Mechanics", 3, "Studies quantum mechanics", "PHYS111", "Lecture", "Professional", "Albert Trebla", 1, "2001-01-01", "Main Building", "Room B");
 
 INSERT INTO enrollment (stud_id, adm_id, coll_id, crs_id, enr_tuition_fee, enr_misc_fee, enr_lab_fee, enr_ppu) VALUES
 (1, 1, 1, 1, 37000, 2500, 10000),
